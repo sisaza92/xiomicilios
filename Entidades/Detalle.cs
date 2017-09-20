a30538849598ba43f174c284047dbc4d7ebd14e0
DIR\aguirre.fabio.andres <faanago>
2017-09-20 16:34:46 -0500

    Entidades categoria, detalle, producto

---
+++ Entidades/Detalle.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Entidades
+{
+    [DataContract]
+    public class Detalle
+    {
+        [DataMember]
+        private long idDetalle;
+        [DataMember]
+        private long idProducto;
+        [DataMember]
+        private long idFactura;
+        [DataMember]
+        private int cantidad;
+        [DataMember]
+        private double precioUnitario;
+        [DataMember]
+        private double subtotal;
+
+        public Detalle(long idDetalle, long idProducto, long idFactura, int cantidad, double precioUnitario, double subtotal)
+        {
+            this.IdDetalle = idDetalle;
+            this.IdProducto = idProducto;
+            this.IdFactura = idFactura;
+            this.Cantidad = cantidad;
+            this.PrecioUnitario = precioUnitario;
+            this.Subtotal = subtotal;
+        }
+
+        public long IdDetalle { get => idDetalle; set => idDetalle = value; }
+        public long IdProducto { get => idProducto; set => idProducto = value; }
+        public long IdFactura { get => idFactura; set => idFactura = value; }
+        public int Cantidad { get => cantidad; set => cantidad = value; }
+        public double PrecioUnitario { get => precioUnitario; set => precioUnitario = value; }
+        public double Subtotal { get => subtotal; set => subtotal = value; }
+    }
+
+
+}
+
+
+
+
---
+++ Entidades/Producto.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Entidades
+{
+    [DataContract]
+    public class Producto
+    {
+        [DataMember]
+        private long idProducto;
+        [DataMember]
+        private long idCategoria;
+        [DataMember]
+        private string nombreProducto;
+        [DataMember]
+        private byte imagenProducto;
+        [DataMember]
+        private int precioProducto;
+        [DataMember]
+        private string descripcion;
+        [DataMember]
+        private DateTime fechaProducto;
+        [DataMember]
+        private bool activoProducto;
+
+        public Producto()
+        {
+
+        }
+
+        public Producto(long idProducto, long idCategoria, string nombreProducto, byte imagenProducto, int precioProducto, string descripcion, DateTime fechaProducto, bool activoProducto)
+        {
+            this.idProducto = idProducto;
+            this.idCategoria = idCategoria;
+            this.nombreProducto = nombreProducto;
+            this.imagenProducto = imagenProducto;
+            this.precioProducto = precioProducto;
+            this.descripcion = descripcion;
+            this.fechaProducto = fechaProducto;
+            this.activoProducto = activoProducto;
+        }
+
+        public long IdProducto { get => idProducto; set => idProducto = value; }
+        public long IdCategoria { get => idCategoria; set => idCategoria = value; }
+        public string NombreProducto { get => nombreProducto; set => nombreProducto = value; }
+        public byte ImagenProducto { get => imagenProducto; set => imagenProducto = value; }
+        public int PrecioProducto { get => precioProducto; set => precioProducto = value; }
+        public string Descripcion { get => descripcion; set => descripcion = value; }
+        public DateTime FechaProducto { get => fechaProducto; set => fechaProducto = value; }
+        public bool ActivoProducto { get => activoProducto; set => activoProducto = value; }
+    }
+}
+
+
+
+
+
+
